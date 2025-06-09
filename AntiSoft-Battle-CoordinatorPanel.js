77483c816392d9417319181a7b4404dfb4e7cc68
AntiSoft <yeugen@gmail.com>
2025-06-10 00:10:22 +0200

    Coordinator Panel - the beginning

---
+++ AntiSoft-Battle-CoordinatorPanel.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         AntiSoft - GWars Battle Coordinator Panel
+// @author       AntiSoft
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Enhanced HUD for GW Coordinators
+// @match        https://www.gwars.io/b*
+// @grant        none
+// ==/UserScript==
+;(() => {
+    console.log(
+        'Здесь будет анализатор синдикатного боя, HUD с информацией о каждом игроке, доска боя и панель с кнопками для координации каждого игрока на каждый ход.'
+    )
+})()
